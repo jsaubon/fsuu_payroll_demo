e31b867521766e31dc5d7341fc5443ab7cf72be6
Joshua Saubon <joshuasaubon@gmail.com>
2020-08-21 09:04:16 +0800

    format

--- resources/js/components/currencyFormat.js
+++ resources/js/components/currencyFormat.js
@@ -1,6 +1,6 @@
 export function currencyFormat(num) {
     if (num) {
-        console.log(num);
+        // console.log(num);
         return num.toFixed(2).replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,");
     } else {
         return 0;
