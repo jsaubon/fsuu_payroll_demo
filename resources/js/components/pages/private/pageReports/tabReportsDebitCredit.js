33898d25b5d9bee6f3867f3ed4ba941ac535b3b0
Joshua Saubon <joshuasaubon@gmail.com>
2020-10-26 14:31:40 +0800

    fix

--- resources/js/components/pages/private/pageReports/tabReportsDebitCredit.js
+++ resources/js/components/pages/private/pageReports/tabReportsDebitCredit.js
@@ -21,7 +21,7 @@ const TabReportsDebitCredit = () => {
         entries: []
     });
     const [tableLoading, setTableLoading] = useState(false);
-
+    const [rawData, setRawData] = useState([]);
     useEffect(() => {
         if (payrollDate || payrollMonthFilter) {
             setTableLoading(true);
@@ -51,6 +51,8 @@ const TabReportsDebitCredit = () => {
                             _data.push(entry);
                         }
                     });
+
+                    setRawData(_data);
 
                     setEmployeeAccountingReports(_data);
                     let employee_filter = [];
@@ -111,6 +113,7 @@ const TabReportsDebitCredit = () => {
                             }
                         }
                     });
+                    console.log(entry_filter);
                     setTotalAmount(_totalAmount);
                     setTableFilters({
                         ...tableFilters,
@@ -131,7 +134,7 @@ const TabReportsDebitCredit = () => {
         return s.charAt(0).toUpperCase() + s.slice(1);
     };
 
-    let columns = [
+    const [columns, setColumns] = useState([
         {
             title: "Client",
             dataIndex: "client",
@@ -225,7 +228,7 @@ const TabReportsDebitCredit = () => {
                 );
             }
         }
-    ];
+    ]);
 
     function onChange(pagination, filters, sorter, extra) {
         let _totalAmount = 0;
@@ -251,10 +254,251 @@ const TabReportsDebitCredit = () => {
         content: () => componentRef.current
     });
 
+    const [filterColumns, setFilterColumns] = useState([]);
+    useEffect(() => {
+        console.log(filterColumns);
+        if (filterColumns.length > 0) {
+            let _columns = [
+                {
+                    title: "Client",
+                    dataIndex: "client",
+                    key: "client",
+                    // render: (text, record) => {
+                    //     return record.client_accounting_entry
+                    //         ? record.client_accounting_entry.client.name
+                    //         : "";
+                    // },
+                    // onFilter: (value, record) =>
+                    //     record.client_accounting_entry &&
+                    //     record.client_accounting_entry.client.name.indexOf(
+                    //         value
+                    //     ) === 0,
+                    // sorter: (a, b) =>
+                    //     a.client_accounting_entry.client.name.length -
+                    //     b.client_accounting_entry.client.name.length,
+                    // sortDirections: ["descend", "ascend"],
+                    filters: [...tableFilters.clients]
+                },
+
+                {
+                    title: "Employee",
+                    dataIndex: "client_employee",
+                    key: "client_employee",
+                    // render: (text, record) => {
+                    //     return record.client_employee.name;
+                    // },
+                    // onFilter: (value, record) =>
+                    //     record.client_employee.name.indexOf(value) === 0,
+                    filters: [...tableFilters.employees]
+                }
+            ];
+            filterColumns.forEach((title, key) => {
+                if (title != "All") {
+                    _columns.push({
+                        title: title,
+                        dataIndex: title.replace(/ /g, "_"),
+                        key: title.replace(/ /g, "_"),
+                        render: (text, record) => {
+                            return currencyFormat(
+                                record[title.replace(/ /g, "_")]
+                            );
+                        }
+                    });
+                }
+            });
+
+            _columns.push({
+                title: "Payroll Date",
+                dataIndex: "client_employee_payroll",
+                key: "client_employee_payroll"
+                // render: (text, record) => {
+                //     return (
+                //         moment(
+                //             record.client_employee_payroll.client_payroll
+                //                 .date_start
+                //         ).format("YYYY-MM-DD") +
+                //         " to " +
+                //         moment(
+                //             record.client_employee_payroll.client_payroll
+                //                 .date_end
+                //         ).format("YYYY-MM-DD")
+                //     );
+                // }
+            });
+
+            setColumns(_columns);
+
+            let _data = [];
+            let _totalAmount = 0;
+            filterColumns.forEach((filter, key) => {
+                let _filter = filter.replace(/ /g, "_");
+                rawData.forEach((entry, index) => {
+                    if (entry.client_accounting_entry.title == filter) {
+                        let data_employee = arrayColumn(
+                            _data,
+                            "client_employee"
+                        );
+
+                        let employee_index = data_employee.indexOf(
+                            entry.client_employee.name
+                        );
+                        if (employee_index !== -1) {
+                            // NAA NA
+
+                            if (_data[employee_index][_filter]) {
+                                _data[employee_index][_filter] =
+                                    _data[employee_index][_filter] +
+                                    entry.amount;
+
+                                if (
+                                    entry.client_accounting_entry.type ==
+                                    "debit"
+                                ) {
+                                    _totalAmount += entry.amount
+                                        ? entry.amount
+                                        : 0;
+                                } else {
+                                    _totalAmount -= entry.amount
+                                        ? entry.amount
+                                        : 0;
+                                }
+                            } else {
+                                _data[employee_index][_filter] = entry.amount;
+
+                                if (
+                                    entry.client_accounting_entry.type ==
+                                    "debit"
+                                ) {
+                                    _totalAmount += entry.amount
+                                        ? entry.amount
+                                        : 0;
+                                } else {
+                                    _totalAmount -= entry.amount
+                                        ? entry.amount
+                                        : 0;
+                                }
+                            }
+                        } else {
+                            let _temp = {
+                                client:
+                                    entry.client_accounting_entry.client.name,
+                                client_employee: entry.client_employee.name,
+                                client_employee_payroll:
+                                    moment(
+                                        entry.client_employee_payroll
+                                            .client_payroll.date_start
+                                    ).format("YYYY-MM-DD") +
+                                    " to " +
+                                    moment(
+                                        entry.client_employee_payroll
+                                            .client_payroll.date_end
+                                    ).format("YYYY-MM-DD"),
+                                [_filter]: entry.amount ? entry.amount : 0
+                            };
+                            if (entry.client_accounting_entry.type == "debit") {
+                                _totalAmount += entry.amount ? entry.amount : 0;
+                            } else {
+                                _totalAmount -= entry.amount ? entry.amount : 0;
+                            }
+
+                            _data.push(_temp);
+                        }
+                    }
+                });
+            });
+            setEmployeeAccountingReports(_data);
+            setTotalAmount(_totalAmount);
+        }
+        return () => {};
+    }, [filterColumns]);
+
+    const arrayColumn = (arr, n) => arr.map(x => x[n]);
+
     return (
         <Card>
             <Title level={4}>
                 Debit/Credit{" "}
+                {rawData.length > 0 && (
+                    <Select
+                        className="pull-right"
+                        style={{ width: "200px" }}
+                        placeholder="Pick Column"
+                        mode="multiple"
+                        onChange={e => {
+                            setFilterColumns(e);
+                        }}
+                    >
+                        <Select.Option value="Basic Pay">
+                            Basic Pay
+                        </Select.Option>
+                        <Select.Option value="Night Premium Pay">
+                            Night Premium Pay
+                        </Select.Option>
+                        <Select.Option value="13th-Month Pay">
+                            13th-Month Pay
+                        </Select.Option>
+                        <Select.Option value="5-Day Service Leave">
+                            5-Day Service Leave
+                        </Select.Option>
+                        <Select.Option value="COLA">COLA</Select.Option>
+                        <Select.Option value="SSS">SSS</Select.Option>
+                        <Select.Option value="PhilHealth">
+                            PhilHealth
+                        </Select.Option>
+                        <Select.Option value="Pag-IBIG">Pag-IBIG</Select.Option>
+                        <Select.Option value="Bond">Bond</Select.Option>
+                        <Select.Option value="LOANS SSS">
+                            LOANS SSS
+                        </Select.Option>
+                        <Select.Option value="LOANS Pag-IBIG">
+                            LOANS Pag-IBIG
+                        </Select.Option>
+                        <Select.Option value="OTHERS C/A">
+                            OTHERS C/A
+                        </Select.Option>
+                        <Select.Option value="OTHERS Canteen">
+                            OTHERS Canteen
+                        </Select.Option>
+                        <Select.Option value="Overtime Pay">
+                            Overtime Pay
+                        </Select.Option>
+                        <Select.Option value="OTHERS Misc">
+                            OTHERS Misc.
+                        </Select.Option>
+                        <Select.Option value="License">License</Select.Option>
+                        <Select.Option value="Separation Pay">
+                            Separation Pay
+                        </Select.Option>
+                        <Select.Option value="Spcl. Hol. Pay">
+                            Spcl. Hol. Pay
+                        </Select.Option>
+                        <Select.Option value="Reg. Hol. Pay">
+                            Reg. Hol. Pay
+                        </Select.Option>
+                        <Select.Option value="SL/VL">SL/VL</Select.Option>
+                        <Select.Option value="LOANS NorthStar">
+                            LOANS NorthStar
+                        </Select.Option>
+                        <Select.Option value="OTHERS Ammos & Accessories">
+                            OTHERS Ammos & Accessories
+                        </Select.Option>
+                        <Select.Option value="Uniform Allowance">
+                            Uniform Allowance
+                        </Select.Option>
+                        <Select.Option value="Night Spcl. Hol. Pay">
+                            Night Spcl. Hol. Pay
+                        </Select.Option>
+                        <Select.Option value="Overtime Spcl. Hol. Pay">
+                            Overtime Spcl. Hol. Pay
+                        </Select.Option>
+                        <Select.Option value="Night Reg. Hol. Pay">
+                            Night Reg. Hol. Pay
+                        </Select.Option>
+                        <Select.Option value="Overtime Reg. Hol. Pay">
+                            Overtime Reg. Hol. Pay
+                        </Select.Option>
+                    </Select>
+                )}
                 <DatePicker
                     style={{ width: "200px" }}
                     placeholder="Pick a Payroll Date"
